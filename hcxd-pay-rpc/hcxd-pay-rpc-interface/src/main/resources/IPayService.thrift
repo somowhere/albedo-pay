45a7626fec89aa4c6836004690b54cc6484ec5cb
lijie <837158334@qq.com>
2017-05-16 10:53:28 +0800

    初始代码提交

--- hcxd-pay-rpc/hcxd-pay-rpc-interface/src/main/resources/IPayService.thrift
+++ hcxd-pay-rpc/hcxd-pay-rpc-interface/src/main/resources/IPayService.thrift
@@ -1,34 +1,13 @@
-namespace java com.qingju.java.mpns.service.thrift
+namespace java com.qingju.java.pay.service.thrift
 
 service PayThriftService {
 
-    string getConnectServerList();
-    i64 getOnlineUserNum(1: string ip);
+    void create(1:PayCreate payCreate);
 
 }
 
-service PushThriftService {
 
-    bool notify(1:string messagePushThrift);
-    void reciveMessage(1:string messageReceiveThrift);
+struct PayCreate {
 
 }
 
-struct MessagePushThrift {
-
-    1: optional string msgId;
-    2: optional list<string> userIds;
-    3: required string content;
-    4: required bool broadcast;
-    5: optional list<string> tags;
-    6: optional string sendCondition;
-    7: optional i64 expireTime;
-
-}
-
-struct MessageReceiveThrift {
-
-    1: required string msgId;
-    2: required string userId;
-
-}
